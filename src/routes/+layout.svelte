346730dfb5401f2961f7b1274982460e264b2346
Ron Caspi <ronroncaspi@gmail.com>
2024-10-09 13:17:32 +0300

    Login Frontend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
 <script>import "../app.css";</script><slot></slot>
+
+<svelte:head>
+    <style>
+        html, body {
+            height: 100%;
+        }
+    </style>
+</svelte:head>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+    <title>HDM Notes Login</title>
+</svelte:head>
+
+<main class="h-full flex items-center justify-center">
+    <div class="p-2 bg-base-300 flex items-center justify-center flex-col rounded-xl">
+        <h1 class="text-2xl mb-2">HDM Notes Login</h1>
+        <label class="m-2">Username: <input type="text" class="input"></label>
+        <label class="m-2">Password: <input type="password" class="input"></label>
+    </div>
+</main>
