0cb6ef99b27b957b2fc60bdcdd2fc0e07440af61
Ron Caspi <ronroncaspi@gmail.com>
2024-10-09 13:54:20 +0300

    Made basic notebook selector

---
+++ src/routes/notebook/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import NotebookCard from "./NotebookCard.svelte"
+</script>
+
+<ul class="flex flex-wrap justify-center h-0 m-4 gap-4 w-full">
+    <NotebookCard title="History"></NotebookCard>
+    <NotebookCard title="History"></NotebookCard>
+    <NotebookCard title="History"></NotebookCard>
+    <NotebookCard title="History"></NotebookCard>
+    <NotebookCard title="History"></NotebookCard>
+    <NotebookCard title="History"></NotebookCard>
+</ul>
