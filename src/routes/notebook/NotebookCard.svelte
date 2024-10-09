36bc4758ed7a8dea9e12bb038eea5032e83ce24a
Ron Caspi <ronroncaspi@gmail.com>
2024-10-09 13:50:53 +0300

    Added NotebookCard element

---
+++ src/routes/notebook/NotebookCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let title = 'Title'
+    export let description = ''
+</script>
+
+<li class="card bg-base-100 w-96 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">{ title }</h2>
+    <p>{ description }</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Edit</button>
+    </div>
+  </div>
+</li>
