29aa78768d8d781e4cf54d120d5e0ae9f6e2cb92
Ron Caspi <ronroncaspi@gmail.com>
2024-12-26 14:14:36 +0200

    Open file option

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,11 +21,34 @@
         }
 
         debug_message = path
+    }
+
+    async function openFile() {
+        let path
+        try {
+            path = await open({
+                multiple: false,
+                directory: false
+            })
+        } catch(e) {
+            debug_message = JSON.stringify(e)
+            return
+        }
+
+        if (path == null) {
+            debug_message = 'User selection canceled'
+            return
+        }
+
+        debug_message = path
     } 
 </script>
 
 <main class="w-full h-full flex justify-center items-center flex-col gap-10">
     <h1 class="text-4xl">Hebrew Markdown Notes</h1>
-    <button class="btn btn-primary" on:click={openNotebook}>Open Notebook</button>
+    <div class="flex gap-4">
+        <button class="btn btn-primary" onclick={openNotebook}>Open Notebook</button>
+        <button class="btn btn-primary" onclick={openFile}>Open File</button>
+    </div>
     <p class="debug text-warning">{ debug_message }</p>
 </main>
